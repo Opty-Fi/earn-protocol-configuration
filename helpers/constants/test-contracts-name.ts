5f06580dd15f21f74b31034169f0aec2c59be784
dhruvinparikh <imsilent93@gmail.com>
2021-12-07 13:29:20 -0500

    refactor(cleanup): non config contracts

--- helpers/constants/test-contracts-name.ts
+++ helpers/constants/test-contracts-name.ts
@@ -1,14 +1,9 @@
 import { DATA_OBJECT } from "../type";
 
 export const TESTING_CONTRACTS: DATA_OBJECT = {
-  TESTING_EMERGENCY_BRAKE: "TestEmergencyBrake",
   TEST_DUMMY_TOKEN: "TestDummyToken",
   TEST_DUMMY_TOKEN_TRANSFER_FEE: "TestDummyTokenTransferFee",
   TEST_DUMMY_EMPTY_CONTRACT: "contracts/mocks/contracts/TestDummyEmptyContract.sol:TestDummyEmptyContract",
   TEST_REGISTRY_NEW_IMPLEMENTATION: "TestRegistryNewImplementation",
   TEST_RISK_MANAGER_NEW_IMPLEMENTATION: "TestRiskManagerNewImplementation",
-  TEST_OPTY_STAKING_RATE_BALANCER_NEW_IMPLEMENTATION: "TestOptyStakingRateBalancerNewImplementation",
-  TEST_VAULT_NEW_IMPLEMENTATION: "TestVaultNewImplementation",
-  TEST_STRATEGY_MANAGER: "TestStrategyManager",
-  TEST_STAKING_RATE_BALANCER: "TestOPTYStakingRateBalancer",
 };
